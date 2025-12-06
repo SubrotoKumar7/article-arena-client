ef7855af2f13a0dd7304386819011c5c9eba1b71
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-06 20:56:31 +0600

    simple register page added

---
+++ src/pages/Auth/Register.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
+import SocialLogin from '../../components/SocialLogin/SocialLogin';
+
+const Register = () => {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const handleRegister = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className='bg-base-300 min-h-screen grid place-items-center'>
+            <div className='md:w-[400px] w-fit bg-white rounded-xl p-7 shadow-2xl'>
+                <h1 className='text-black font-bold text-2xl text-center pb-3 pt-4'>Register Now</h1>
+                <form onSubmit={handleSubmit(handleRegister)}>
+                    <fieldset className="fieldset">
+
+                        {/* name */}
+                        <label className="label">Name</label>
+                        <input type="text" {...register("name", {required: {value: true, message: "Name is required"}})} className="input w-full" placeholder="Name" />
+                        {errors?.name && <p className='text-red-500'>{errors?.name.message}</p>}
+
+                        {/* photo */}
+                        <label className="label">Upload Photo</label>
+                        <input type="file" {...register("image", {required: {value: true, message: "Image is required"}})} className="file-input w-full" />
+                        {errors?.image && <p className='text-red-500'>{errors?.image.message}</p>}
+
+                        {/* email */}
+                        <label className="label">Email</label>
+                        <input type="email" {...register("email", {required: {value: true, message: "Email is required"}})} className="input w-full" placeholder="Email" />
+                        {errors?.email && <p className='text-red-500'>{errors?.email.message}</p>}
+
+                        {/* password */}
+                        <label className="label">Password</label>
+                        <input type="password" {...register("password", {required: {value: true, message: "Password is required"}, pattern: {value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/, message: "Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 special character and length at least 6 characters"}})} className="input w-full" placeholder="Password" />
+                        {errors?.password && <p className='text-red-500'>{errors?.password.message}</p>}
+
+
+                        <button className="btn btn-neutral mt-2">Register</button>
+                    </fieldset>
+                </form>
+                <p className='my-3 font-medium text-center'>Already have an account? <Link className='text-primary font-semibold' to='/login'>Login</Link></p>
+                <p className='text-center font-medium text-sm'>Or With</p>
+                <div className='flex justify-center my-2'></div>
+                <div className=''>
+                    <SocialLogin></SocialLogin>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Root from "../layout/Root";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
     {
         path: '/login',
         Component: Login
+    },
+    {
+        path: '/register',
+        Component: Register
     }
 ]);
 
