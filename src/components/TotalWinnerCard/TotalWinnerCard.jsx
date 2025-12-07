9f62c8a57f0f82d70986724214e9dc237eceeef7
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-07 20:04:26 +0600

    total price and total winner card added

---
+++ src/components/TotalWinnerCard/TotalWinnerCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TotalWinnerCard = () => {
+    return (
+        <div className='p-2 bg-linear-to-r from-blue-500 via-blue-700 to-blue-600 text-white shadow-2xl shadow-gray-600 rounded grid place-items-center'>
+            <div className='text-center'>
+                <h1 className='text-4xl mb-2 font-bold'>Total Winner</h1>
+                <p className='text-4xl'>5000+</p>
+            </div>
+        </div>
+    );
+};
+
+export default TotalWinnerCard;
--- src/pages/Home/RecentWinner/RecentWinner.jsx
+++ src/pages/Home/RecentWinner/RecentWinner.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from '../../../components/Shared/Container/Container';
 import Heading from '../../../components/Shared/Heading/Heading';
 import WinnerCard from '../../../components/WinnerCard/WinnerCard';
+import TotalWinnerCard from '../../../components/TotalWinnerCard/TotalWinnerCard';
+import TotalPriceMoney from '../../../components/TotalPriceMoney/TotalPriceMoney';
 
 const RecentWinner = () => {
     return (
@@ -9,7 +11,9 @@ const RecentWinner = () => {
             <Container>
                 <Heading position={'text-center'} title={'Meet Our Recent Champions'} subtitle={'Celebrating the outstanding achievements of our winners. See who rose to the top and claimed the prize!'}></Heading>
                 <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10 gap-5 pt-10'>
+                    <TotalWinnerCard></TotalWinnerCard>
                     <WinnerCard></WinnerCard>
+                    <TotalPriceMoney></TotalPriceMoney>
                 </div>
             </Container>
         </div>
