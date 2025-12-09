020dee3b62b2c15da65936e5e43ad4b69d43a6ad
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-09 21:54:32 +0600

    axios hooks added

---
+++ src/hooks/useAxiosSecured.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import useAuth from './useAuth';
+import { useEffect } from 'react';
+import {useNavigate} from 'react-router'
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+const useAxiosSecured = () => {
+    const {user, logoutUser} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        const reqInterceptor = axiosSecure.interceptors.request.use((config)=> {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`
+            return config;
+        })
+
+        const resInterceptor = axiosSecure.interceptors.response.use((res)=>{
+            return res;
+        }, (err)=> {
+            const statusCode = err.status;
+            if(statusCode === 401){
+                logoutUser()
+                .then(()=> {
+                    navigate('/login');
+                })
+            }
+
+
+            if(statusCode === 403){
+                navigate('/');
+            }
+
+            return Promise.reject(err);
+        })
+
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+
+    }, [user, logoutUser, navigate])
+
+    return axiosSecure;
+};
+
+export default useAxiosSecured;
