c35539bf44219990573e5bc741b0bd4792145199
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-13 20:30:48 +0600

    joined contest page design added

--- src/pages/Dashboard/User/JoinedContest/JoinedContest.jsx
+++ src/pages/Dashboard/User/JoinedContest/JoinedContest.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import DashboardHeading from '../../../../components/Shared/DashboardHeading/DashboardHeading';
 
 const JoinedContest = () => {
     return (
         <div>
-            <h1>Joined Contest</h1>
+            <DashboardHeading title={'Joined Contest'}></DashboardHeading>
+            <div className='py-10'>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                            <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>Contest Name</th>
+                                <th>Category</th>
+                                <th>Prize Money</th>
+                                <td>Entry Fee</td>
+                                <th>Payment Status</th>
+                                <th>Contest Deadline</th>
+                            </tr>
+                            </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Global Warming Awareness Contest</td>
+                                <td>Environment</td>
+                                <td>2000</td>
+                                <td>50</td>
+                                <td>Paid</td>
+                                <td>20/1/2026</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
