92a1330d294ff2b217cf747dd6878833744ec0be
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-14 15:58:51 +0600

    role base dashboard home show

---
+++ src/pages/Dashboard/Creator/CreatorHome/CreatorHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatorHome = () => {
+    return (
+        <div>
+            <h1>Creator Dashboard Home</h1>
+        </div>
+    );
+};
+
+export default CreatorHome;
--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import useRole from '../../../hooks/useRole';
+import AdminHome from '../Admin/AdminHome/AdminHome';
+import Loader from '../../../components/Loader/Loader';
+import CreatorHome from '../Creator/CreatorHome/CreatorHome';
+import UserHome from '../User/UserHome/UserHome';
 
 const DashboardHome = () => {
-    return (
-        <div>
-            <h1>Dashboard Home</h1>
-        </div>
-    );
+    const {role: {role}, roleLoading} = useRole();
+
+    if(roleLoading){
+        return <Loader></Loader>
+    }
+
+    if(role === 'admin'){
+        return <AdminHome></AdminHome>
+    }
+
+    if(role === 'creator'){
+        return <CreatorHome></CreatorHome>
+    }
+
+    if(role === 'user'){
+        return <UserHome></UserHome>
+    }
+
 };
 
 export default DashboardHome;
