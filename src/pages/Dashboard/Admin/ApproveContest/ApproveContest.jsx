74988918e324967e4e9ce0bbcbcb27910eba85b6
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-17 16:47:39 +0600

    show pending contest in table

--- src/pages/Dashboard/Admin/ApproveContest/ApproveContest.jsx
+++ src/pages/Dashboard/Admin/ApproveContest/ApproveContest.jsx
@@ -1,15 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import DashboardHeading from '../../../../components/Shared/DashboardHeading/DashboardHeading';
+import useAxiosSecured from '../../../../hooks/useAxiosSecured';
+import { useQuery } from '@tanstack/react-query';
 
 const ApproveContest = () => {
     const contestModalRef = useRef();
+    const [selectedContest, setSelectedContest] = useState({});
+    const axiosSecure = useAxiosSecured();
+    const {data: pendingContest = []} = useQuery({
+        queryKey: ['pending-contest'],
+        queryFn: async()=> {
+            const res = await axiosSecure.get('/pending-contest');
+            return res.data;
+        }
+    })
 
-    const handleModal = () => {
+
+    const handleModal = (contest) => {
         contestModalRef.current.showModal();
+        setSelectedContest(contest);
     }
 
     return (
-        <div>
+        <div className='mb-20'>
             <DashboardHeading title={'Manage Contest'}></DashboardHeading>
             <div className='py-10'>
                 <div className="overflow-x-auto">
@@ -26,32 +39,34 @@ const ApproveContest = () => {
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <td>1</td>
+                            {
+                                pendingContest.map((contest, index)=> <tr key={contest._id}>
+                                <td>{index + 1}</td>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
                                             <div className="mask mask-squircle h-12 w-12">
                                                 <img
-                                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                                src={contest?.creatorPhoto}
                                                 alt="user profile image" />
                                             </div>
                                         </div>
                                         <div>
-                                            <div className="font-bold">John Doe</div>
-                                            <p>john@mail.com</p>
+                                            <div className="font-bold">{contest?.creatorName}</div>
+                                            <p>{contest?.creatorEmail}</p>
                                         </div>
                                     </div>
                                 </td>
-                                <td>Future of AI in Daily Life</td>
-                                <td>AI</td>
-                                <td><button onClick={handleModal} className="btn btn-primary btn-xs">details</button></td>
+                                <td>{contest?.contestName}</td>
+                                <td>{contest?.category}</td>
+                                <td><button onClick={()=> handleModal(contest)} className="btn btn-primary btn-xs">details</button></td>
                                 <td className='space-x-1'>
                                     <button className="btn bg-green-500 btn-xs">Confirm</button>
                                     <button className="btn btn-warning btn-xs">Reject</button>
                                     <button className="btn bg-red-500 text-white btn-xs">Delete</button>
                                 </td>
-                            </tr>
+                            </tr>)
+                            }
                         </tbody>
                     </table>
                 </div>
@@ -60,16 +75,15 @@ const ApproveContest = () => {
             {/* modal */}
             <dialog ref={contestModalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-center mb-2 text-lg">Contest Name</h3>
-                    <img className='w-full h-[250px]' src="https://img.freepik.com/free-photo/futuristic-ai-chip-circuit-board_23-2151977487.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                    <h3 className="font-bold text-center mb-2 text-lg">{selectedContest.contestName}</h3>
+                    <img className='w-full h-[250px]' src={selectedContest.image} alt="" />
                     <div className='mt-5 space-y-1'>
-                        <div><span className='font-semibold'>Price:</span> <span>50</span></div>
-                        <div><span className='font-semibold'>Prize Money:</span> <span>5000</span></div>
-                        <div><span className='font-semibold'>Deadline:</span> <span>30/12/2025</span></div>
-                        <div><span className='font-semibold'>Category:</span> <span>AI</span></div>
-                        <div><span className='font-semibold'>Created At:</span> <span>20/12/2025</span></div>
-                        <div><span className='font-semibold'>Description:</span> <span>Participants must write an evidence-based article explaining how healthy habits are formed and how individuals can build long-term discipline. Articles may explore habit psychology, neuroscience, lifestyle design, and real-life success strategies. Your writing should inspire readers to improve their lives while staying grounded in scientific principles</span></div>
-
+                        <div><span className='font-semibold'>Price:</span> <span>{selectedContest.price}</span></div>
+                        <div><span className='font-semibold'>Prize Money:</span> <span>{selectedContest.prizeMoney}</span></div>
+                        <div><span className='font-semibold'>Deadline:</span> <span>{new Date(selectedContest.deadline).toDateString()}</span></div>
+                        <div><span className='font-semibold'>Category:</span> <span>{selectedContest.category}</span></div>
+                        <div><span className='font-semibold'>Created At:</span> <span>{new Date(selectedContest.createdAt).toDateString()}</span></div>
+                        <div><span className='font-semibold'>Description:</span> <span>{selectedContest.description}</span></div>
                     </div>
 
                     <div className="modal-action">
