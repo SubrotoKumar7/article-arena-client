6e753300b4ebaf46e7061262398d06fabdef5a85
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-19 12:36:57 +0600

    contest submit button added

--- src/pages/Dashboard/User/JoinedContest/JoinedContest.jsx
+++ src/pages/Dashboard/User/JoinedContest/JoinedContest.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import DashboardHeading from '../../../../components/Shared/DashboardHeading/DashboardHeading';
 
 const JoinedContest = () => {
+    const modalRef = useRef();
+
+    const handleModal = () => {
+        modalRef.current.showModal();
+    }
+
     return (
         <div>
             <DashboardHeading title={'Joined Contest'}></DashboardHeading>
@@ -17,6 +23,7 @@ const JoinedContest = () => {
                                 <td>Entry Fee</td>
                                 <th>Payment Status</th>
                                 <th>Contest Deadline</th>
+                                <th>Submit Task</th>
                             </tr>
                             </thead>
                         <tbody>
@@ -28,11 +35,32 @@ const JoinedContest = () => {
                                 <td>50</td>
                                 <td>Paid</td>
                                 <td>20/1/2026</td>
+                                <td>
+                                    <button onClick={handleModal} className='btn btn-sm btn-primary'>Submit</button>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
+
+            <dialog ref={modalRef}  className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Submit Contest</h3>
+                    <div>
+                        <form className='w-full p-2'>
+                            <fieldset className="fieldset">
+                                <legend className="fieldset-legend">Your Contest</legend>
+                                <textarea className="textarea h-24 w-full" placeholder="write your contest here"></textarea>
+                            </fieldset>
+                            <div className='flex gap-2 mt-2'>
+                                <button type='submit' className='btn btn-primary'>Submit</button>
+                                <button type='button' onClick={()=> modalRef.current.close()} className='btn btn-secondary'>Close</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
