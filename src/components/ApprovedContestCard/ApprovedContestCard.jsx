77e976941c0715dee32ede226607e86e7fb295ad
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-20 23:53:31 +0600

    show approved contest for declare winner

--- src/components/ApprovedContestCard/ApprovedContestCard.jsx
+++ src/components/ApprovedContestCard/ApprovedContestCard.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const ApprovedContestCard = () => {
+const ApprovedContestCard = ({contest}) => {
+
     return (
         <div className='my-5'>
             <div className='flex gap-5 flex-col md:flex-row items-center justify-between w-full md:w-10/12 p-3 mx-auto rounded border border-gray-100 shadow-2xl'>
                 <div className='flex flex-col md:flex-row gap-2'>
-                    <img className='rounded' src="https://placehold.co/150x120" alt="contest image" />
+                    <img className='rounded w-full h-60 md:w-50 md:h-35' src={contest.image} alt="contest image" />
                     <div className='flex-1'>
-                        <h1 className='text-xl whitespace-nowrap font-semibold mb-1'>Contest Name</h1>
+                        <h1 className='text-xl whitespace-nowrap font-semibold mb-1'>{contest.contestName}</h1>
                         <div>
-                            <p><span className='font-medium'>Total Participant:</span> <span>30</span></p>
-                            <p><span className='font-medium'>Category:</span> <span>Wid Life</span></p>
-                            <p><span className='font-medium'>Price Money:</span> <span>1000$</span></p>
+                            <p><span className='font-medium'>Total Participant:</span> <span>{contest.participant}</span></p>
+                            <p><span className='font-medium'>Category:</span> <span>{contest.category}</span></p>
+                            <p><span className='font-medium'>Price Money:</span> <span>{contest.prizeMoney}$</span></p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <Link to={'/dashboard/all-participant/35'} className='btn btn-primary'>view participant</Link>
+                    <Link to={`/dashboard/all-participant/${contest._id}`} className='btn btn-primary'>view participant</Link>
                 </div>
             </div>
         </div>
--- src/pages/Dashboard/Creator/DeclareWinner/DeclareWinner.jsx
+++ src/pages/Dashboard/Creator/DeclareWinner/DeclareWinner.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import DashboardHeading from '../../../../components/Shared/DashboardHeading/DashboardHeading';
 import ApprovedContestCard from '../../../../components/ApprovedContestCard/ApprovedContestCard';
+import useAxiosSecured from '../../../../hooks/useAxiosSecured';
+import { useQuery } from '@tanstack/react-query';
+import useAuth from '../../../../hooks/useAuth';
+import Loader from '../../../../components/Loader/Loader';
 
 const DeclareWinner = () => {
+    const axiosSecure = useAxiosSecured();
+    const {user} = useAuth();
+    const {data: myContest = [], isLoading} = useQuery({
+        queryKey: ['my-contest', user?.email],
+        queryFn: async() => {
+            const res = await axiosSecure.get('/my-contest?status=approved');
+            return res.data;
+        }
+    })
+
+    if(isLoading){
+        return <Loader></Loader>
+    }
+
     return (
         <div>
             <DashboardHeading title={'My Approved Contest'}></DashboardHeading>
-            <div className='my-10'>
-                <ApprovedContestCard></ApprovedContestCard>
-                <ApprovedContestCard></ApprovedContestCard>
-                <ApprovedContestCard></ApprovedContestCard>
+            <div className='mt-10 mb-20'>
+                {
+                    myContest.length > 0 ?
+                    myContest.map(contest => <ApprovedContestCard key={contest._id} contest={contest}></ApprovedContestCard>)
+                    :
+                    <div className='flex items-center justify-center h-[70vh]'>
+                        <h1 className='text-2xl font-semibold'>You have no approved contests.</h1>
+                    </div>
+                }                
+
             </div>
         </div>
     );
