3a577a783b33abed971615f06ae84a06730bdcc6
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-21 23:30:34 +0600

    show recent winner

--- src/components/WinnerCard/WinnerCard.jsx
+++ src/components/WinnerCard/WinnerCard.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
-import { IoMdTrophy } from 'react-icons/io';
 
-const WinnerCard = () => {
+const WinnerCard = ({latestWinner}) => {
+    const info = latestWinner;
+
     return (
         <div className='p-2 bg-white text-black shadow-2xl shadow-gray-600 rounded'>
             <div>
-                <img className='w-full h-[250px]' src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="contest winner images" />
+                <img className='w-full h-[250px] object-contain' src={info.winnerPhoto} alt="contest winner images" />
             </div>
             <div className='flex justify-between mt-5'>
                 <div className='py-2 flex-1 pl-1'>
-                    <h1 className='text-xl font-bold'>John Doe</h1>
-                    <p className='italic my-1'>john@mail.com</p>
-                    <p><span className='font-semibold'>Winning Date:</span> 11/12/2025</p>
+                    <h1 className='text-xl font-bold'>{info.winnerName}</h1>
+                    <p className='italic my-1'>{info.winnerEmail}</p>
+                    <p><span className='font-semibold'>Winning Date:</span> {new Date(info.createdAt).toDateString()}</p>
                 </div>
                 <div className="h-30 border-l-4 border-gray-300"></div>
                 <div className='flex-1 pl-3'>
                     <div className='flex items-center gap-1'>
                         <BsCurrencyDollar size={25} fill='blue' />
-                        <span className='font-bold text-xl'>1600</span>
+                        <span className='font-bold text-xl'>{info.prizeMoney}</span>
                     </div>
-                    <p className='mt-2'><span className='font-semibold'>Winning Contest:</span> Global Warming Awareness Contest</p>
+                    <p className='mt-2'><span className='font-semibold'>Winning Contest:</span> {info.contestName}</p>
                 </div>
             </div>
         </div>
--- src/pages/Home/RecentWinner/RecentWinner.jsx
+++ src/pages/Home/RecentWinner/RecentWinner.jsx
@@ -4,15 +4,33 @@ import Heading from '../../../components/Shared/Heading/Heading';
 import WinnerCard from '../../../components/WinnerCard/WinnerCard';
 import TotalWinnerCard from '../../../components/TotalWinnerCard/TotalWinnerCard';
 import TotalPriceMoney from '../../../components/TotalPriceMoney/TotalPriceMoney';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecured from '../../../hooks/useAxiosSecured';
 
 const RecentWinner = () => {
+    const axiosSecure = useAxiosSecured();
+    const {data: latestWinner = []} = useQuery({
+        queryKey: ['recentWinner'],
+        queryFn: async() => {
+            const res = await axiosSecure.get('/latest-winner');
+            return res.data[0];
+        }
+    });
+
     return (
         <div className='py-10'>
             <Container>
                 <Heading position={'text-center'} title={'Meet Our Recent Champions'} subtitle={'Celebrating the outstanding achievements of our winners. See who rose to the top and claimed the prize!'}></Heading>
                 <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10 gap-5 pt-10'>
                     <TotalWinnerCard></TotalWinnerCard>
-                    <WinnerCard></WinnerCard>
+                    {
+                        latestWinner ?
+                        <WinnerCard latestWinner={latestWinner}></WinnerCard>
+                        :
+                        <div className='p-2 bg-linear-to-r from-blue-500 via-blue-700 to-blue-600 text-white shadow-2xl shadow-gray-600 rounded grid place-items-center'>
+                            <h1 className='text-3xl font-bold'>No Recent Winner</h1>
+                        </div>
+                    }
                     <TotalPriceMoney></TotalPriceMoney>
                 </div>
             </Container>
