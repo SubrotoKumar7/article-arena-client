ba6351c52074f72975657b6982fa2b17ba24db23
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-22 00:41:14 +0600

    winning contest card show

--- src/pages/Dashboard/User/WinningContest/WinningContest.jsx
+++ src/pages/Dashboard/User/WinningContest/WinningContest.jsx
@@ -1,6 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecured from '../../../../hooks/useAxiosSecured';
+import Loader from '../../../../components/Loader/Loader';
 
 const WinningContest = () => {
+    const axiosSecure = useAxiosSecured();
+    const {data: winningContest = [], isLoading} = useQuery({
+        queryKey: ['winningContest'],
+        queryFn: async() => {
+            const res = await axiosSecure.get('/winning-contest');
+            return res.data;
+        }
+    });
+
+    if(isLoading){
+        return <Loader></Loader>
+    }
+
     return (
         <div className='p-5'>
             <div>
@@ -10,41 +26,28 @@ const WinningContest = () => {
                 </div>
 
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
-                    <div className='shadow-2xl p-5 rounded border border-gray-200'>
-                        <h1 className='text-xl mb-3 font-semibold'>Tech Article Writing Contest</h1>
-                        <div className='space-y-1.5'>
-                            <div>🏷️ Category: <span>Technology</span></div>
-                            <div className='mb-1'>📅 End Date: <span>12 January 2026</span></div>
+                    {   
+                        winningContest.length > 0 ?
+                        winningContest.map(win => <div key={win._id} className='shadow-2xl p-5 rounded border border-gray-200'>
+                            <div className='text-center'>
+                                <h1 className='text-3xl font-semibold py-1'>Congratulations</h1>
+                                <div className='my-2 text-2xl'>🎉🎉🎉</div>
+                            </div>
+                            <div className='space-y-1.5'>
+                                <h1 className='text-xl mb-3 font-semibold'>{win.contestName}</h1>
+                                <div className='mb-1'>📅 Winning Date: <span>{new Date(win.createdAt).toDateString()}</span></div>
+                            </div>
+                            <div className='flex items-center justify-between pt-3'>
+                                <div className='text-xl text-green-700 font-bold'>💰 <span>${win.prizeMoney}</span></div>
+                                <div className='font-semibold'>🏆 Winner</div>
+                            </div>
+                        </div>)
+                        
+                        :
+                        <div className='h-[70vh] flex items-center justify-center col-end-1 md:col-span-3'>
+                            <h1 className='text-2xl font-semibold text-center'>You have not won any content.</h1>
                         </div>
-                        <div className='flex items-center justify-between pt-3'>
-                            <div className='text-xl text-green-700 font-bold'>💰 <span>$1000</span></div>
-                            <div className='font-semibold'>🏆 Winner</div>
-                        </div>
-                    </div>
-
-                    <div className='shadow-2xl p-5 rounded border border-gray-200'>
-                        <h1 className='text-xl mb-3 font-semibold'>Tech Article Writing Contest</h1>
-                        <div className='space-y-1.5'>
-                            <div>🏷️ Category: <span>Technology</span></div>
-                            <div className='mb-1'>📅 End Date: <span>12 January 2026</span></div>
-                        </div>
-                        <div className='flex items-center justify-between pt-3'>
-                            <div className='text-xl text-green-700 font-bold'>💰 <span>$1000</span></div>
-                            <div className='font-semibold'>🏆 Winner</div>
-                        </div>
-                    </div>
-
-                    <div className='shadow-2xl p-5 rounded border border-gray-200'>
-                        <h1 className='text-xl mb-3 font-semibold'>Tech Article Writing Contest</h1>
-                        <div className='space-y-1.5'>
-                            <div>🏷️ Category: <span>Technology</span></div>
-                            <div className='mb-1'>📅 End Date: <span>12 January 2026</span></div>
-                        </div>
-                        <div className='flex items-center justify-between pt-3'>
-                            <div className='text-xl text-green-700 font-bold'>💰 <span>$1000</span></div>
-                            <div className='font-semibold'>🏆 Winner</div>
-                        </div>
-                    </div>
+                    }
                 </div>
             </div>
         </div>
